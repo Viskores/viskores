dab668b71ac0c336852b1e41d0d7f152fb62411e
Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>
2025-02-06 19:08:23 -0500

    thirdparties: update to new tags

--- viskores/thirdparty/lcl/update.sh
+++ viskores/thirdparty/lcl/update.sh
@@ -8,7 +8,7 @@ readonly name="lcl"
 readonly ownership="Lightweight Cell Library Upstream <kwrobot@kitware.com>"
 readonly subtree="viskores/thirdparty/$name/viskores$name"
 readonly repo="https://gitlab.kitware.com/vtk/lcl.git"
-readonly tag="master"
+readonly tag="for/viskores-20250206-g58a4bf3"
 readonly paths="
 lcl
 LICENSE.md
--- viskores/thirdparty/lodepng/update.sh
+++ viskores/thirdparty/lodepng/update.sh
@@ -8,7 +8,7 @@ readonly name="lodepng"
 readonly ownership="LodePNG Upstream <kwrobot@kitware.com>"
 readonly subtree="viskores/thirdparty/$name/viskores$name"
 readonly repo="https://gitlab.kitware.com/third-party/$name.git"
-readonly tag="for/viskores"
+readonly tag="for/viskores-20250206-g690034b"
 readonly paths="
 LICENSE
 lodepng.cpp
