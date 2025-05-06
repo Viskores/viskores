f6e8cfab051cb361c14635ab495e792e267691ea
Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>
2025-05-06 12:35:41 -0400

    thirdparty: update DIY

--- viskores/thirdparty/diy/update.sh
+++ viskores/thirdparty/diy/update.sh
@@ -8,7 +8,7 @@ readonly name="diy"
 readonly ownership="Diy Upstream <kwrobot@kitware.com>"
 readonly subtree="viskores/thirdparty/$name/viskores$name"
 readonly repo="https://gitlab.kitware.com/third-party/diy2.git"
-readonly tag="for/viskores-20250321-g40ea01f9"
+readonly tag="for/viskores-20250506-gad6b15f"
 readonly paths="
 cmake
 include
