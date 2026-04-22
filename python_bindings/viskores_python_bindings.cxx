9f76e8640ba54351ab5b036c23810e2f9956329e
Gunther H. Weber <GHWeber@lbl.gov>
2026-04-22 15:47:19 -0700

    Address initial Python bindings review feedback

--- python_bindings/viskores_python_bindings.cxx
+++ python_bindings/viskores_python_bindings.cxx
@@ -6,16 +6,6 @@
 //  Certificate of Origin Version 1.1 (DCO 1.1) as stated in DCO.txt.
 //============================================================================
 
-//============================================================================
-//  Copyright (c) Kitware, Inc.
-//  All rights reserved.
-//  See LICENSE.txt for details.
-//
-//  This software is distributed WITHOUT ANY WARRANTY; without even
-//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-//  PURPOSE.  See the above copyright notice for more information.
-//============================================================================
-
 #include <Python.h>
 #include <structmember.h>
 
