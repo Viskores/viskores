9fad703274f9b3098cb41f2c7a04aac001094a99
Gunther H. Weber <GHWeber@lbl.gov>
2026-04-24 16:51:06 -0700

    Add missing Python test license headers

--- python_bindings/testing/unit_test_cell_set_class.py
+++ python_bindings/testing/unit_test_cell_set_class.py
@@ -1,3 +1,11 @@
+##============================================================================
+##  The contents of this file are covered by the Viskores license. See
+##  LICENSE.txt for details.
+##
+##  By contributing to this file, all contributors agree to the Developer
+##  Certificate of Origin Version 1.1 (DCO 1.1) as stated in DCO.txt.
+##============================================================================
+
 from viskores import CELL_SHAPE_HEXAHEDRON, CELL_SHAPE_TRIANGLE
 from viskores import create_uniform_dataset
 from viskores.cont import CellSet, DataSetBuilderExplicit
