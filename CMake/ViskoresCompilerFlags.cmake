Viskores/viskores

commit dbb0f58006a936ef51c0cf61064044281c090412

Use compliant MSVC preprocessor for device code
MSVC has some behavior in their "traditional" preprocessor that is not compliant
with the C99 and C++11 standards. Starting in MSVC 19 (the current earliest
version supported by Viskores), MSVC provides a "compliant" preprocessor that
does meet C and C++ standards. This is enabled with the `/Zc:preprocessor`
command line flag.

Some device libraries (in particular, Thrust) require this compliant preprocess
to be used for compatibility. To support that, Viskores now adds the
`/Zc:preprocessor` flag to compilations of device code. This is done by adding
the compile option to the `viskores_exec` interface library.

Fixes: #276

CMake/ViskoresCompilerFlags.cmake
@@ -81,6 +81,14 @@ if(VISKORES_COMPILER_IS_MSVC)
   endif()
 endif()
 
+# Use a standard-compliant preprocessor (required for recent versions of Thrust).
+if(VISKORES_COMPILER_IS_MSVC)
+  target_compile_options(viskores_compiler_flags INTERFACE $<$<COMPILE_LANGUAGE:CXX>:/Zc:preprocessor>)
+  if(TARGET viskores_cuda)
+    target_compile_options(viskores_compiler_flags INTERFACE $<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler="/Zc:preprocessor")
+  endif()
+endif()
+
 # Setup the include directories that are needed for viskores
 target_include_directories(viskores_compiler_flags INTERFACE
   $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>

commit 5fc4010a6dbf2835f654668690af33b745383c54

Fix CMake generator statement
There was an error in the CMake generator statement for adding the flag
for a compliant preprocessor to the MSVC Cuda build. The closing `>` was
missing. This error was introduced in commit
dbb0f58006a936ef51c0cf61064044281c090412 (GitHub PR #277).

CMake/ViskoresCompilerFlags.cmake
@@ -85,7 +85,7 @@ endif()
 if(VISKORES_COMPILER_IS_MSVC)
   target_compile_options(viskores_compiler_flags INTERFACE $<$<COMPILE_LANGUAGE:CXX>:/Zc:preprocessor>)
   if(TARGET viskores_cuda)
-    target_compile_options(viskores_compiler_flags INTERFACE $<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler="/Zc:preprocessor")
+    target_compile_options(viskores_compiler_flags INTERFACE $<$<COMPILE_LANGUAGE:CUDA>:-Xcompiler="/Zc:preprocessor">)
   endif()
 endif()
 