94d0fee668774ee51bb693f19e9a1a8b466ea990
Kenneth Moreland <morelandkd@ornl.gov>
2026-06-07 02:15:27 -0400

    Add an SSIM filter
    The Structural Similarity Index Measure (SSIM) is a useful tool for comparing
    images. SSIM was designed to better capture visual differences of images than
    simple difference or L-norm measures. Additioinally, the SSIM metric tends to be
    less sensitive to minor differences in rendering and is thus less likely to be
    tripped up by, for example, lines drawn a pixel off. The new SSIM filter
    performs this comparison for images.
    
    This commit coauthored by Codex AI agent.

---
+++ viskores/filter/image_processing/SSIM.cxx
@@ -0,0 +1,466 @@
+//============================================================================
+//  The contents of this file are covered by the Viskores license. See
+//  LICENSE.txt for details.
+//
+//  By contributing to this file, all contributors agree to the Developer
+//  Certificate of Origin Version 1.1 (DCO 1.1) as stated in DCO.txt.
+//============================================================================
+
+#include <viskores/filter/image_processing/SSIM.h>
+
+#include <viskores/Math.h>
+#include <viskores/cont/Algorithm.h>
+#include <viskores/cont/ErrorFilterExecution.h>
+#include <viskores/cont/Invoker.h>
+#include <viskores/cont/UncertainCellSet.h>
+#include <viskores/worklet/WorkletMapField.h>
+#include <viskores/worklet/WorkletPointNeighborhood.h>
+
+#include <limits>
+#include <type_traits>
+
+namespace
+{
+
+struct SSIMStatistics
+{
+  viskores::Float64 SumPrimary = 0;
+  viskores::Float64 SumSecondary = 0;
+  viskores::Float64 SumPrimarySquared = 0;
+  viskores::Float64 SumSecondarySquared = 0;
+  viskores::Float64 SumProduct = 0;
+  viskores::Float64 Weight = 0;
+
+  VISKORES_EXEC_CONT void Add(viskores::Float64 primary,
+                              viskores::Float64 secondary,
+                              viskores::Float64 weight)
+  {
+    this->SumPrimary += primary * weight;
+    this->SumSecondary += secondary * weight;
+    this->SumPrimarySquared += primary * primary * weight;
+    this->SumSecondarySquared += secondary * secondary * weight;
+    this->SumProduct += primary * secondary * weight;
+    this->Weight += weight;
+  }
+
+  VISKORES_EXEC_CONT SSIMStatistics operator+(const SSIMStatistics& rhs) const
+  {
+    SSIMStatistics result;
+    result.SumPrimary = this->SumPrimary + rhs.SumPrimary;
+    result.SumSecondary = this->SumSecondary + rhs.SumSecondary;
+    result.SumPrimarySquared = this->SumPrimarySquared + rhs.SumPrimarySquared;
+    result.SumSecondarySquared = this->SumSecondarySquared + rhs.SumSecondarySquared;
+    result.SumProduct = this->SumProduct + rhs.SumProduct;
+    result.Weight = this->Weight + rhs.Weight;
+    return result;
+  }
+};
+
+class SSIMComponentNeighborhood : public viskores::worklet::WorkletPointNeighborhood
+{
+public:
+  using ControlSignature = void(CellSetIn, FieldInNeighborhood, FieldInNeighborhood, FieldOut);
+  using ExecutionSignature = void(_2, _3, Boundary, _4);
+  using InputDomain = _1;
+
+  VISKORES_CONT
+  explicit SSIMComponentNeighborhood(viskores::IdComponent patchRadius)
+    : PatchRadius(patchRadius)
+  {
+  }
+
+  template <typename PrimaryFieldPortalType, typename SecondaryFieldPortalType>
+  VISKORES_EXEC void operator()(
+    const viskores::exec::FieldNeighborhood<PrimaryFieldPortalType>& primary,
+    const viskores::exec::FieldNeighborhood<SecondaryFieldPortalType>& secondary,
+    const viskores::exec::BoundaryState& boundary,
+    SSIMStatistics& statistics) const
+  {
+    statistics = SSIMStatistics{};
+    auto minIndices = boundary.MinNeighborIndices(this->PatchRadius);
+    auto maxIndices = boundary.MaxNeighborIndices(this->PatchRadius);
+    const viskores::IdComponent radiusSquared = this->PatchRadius * this->PatchRadius;
+
+    for (viskores::IdComponent k = minIndices[2]; k <= maxIndices[2]; ++k)
+    {
+      for (viskores::IdComponent j = minIndices[1]; j <= maxIndices[1]; ++j)
+      {
+        for (viskores::IdComponent i = minIndices[0]; i <= maxIndices[0]; ++i)
+        {
+          const viskores::IdComponent distanceSquared = (i * i) + (j * j) + (k * k);
+          if (distanceSquared <= radiusSquared)
+          {
+            statistics.Add(static_cast<viskores::Float64>(primary.Get(i, j, k)),
+                           static_cast<viskores::Float64>(secondary.Get(i, j, k)),
+                           this->ComputeWeight(distanceSquared));
+          }
+        }
+      }
+    }
+  }
+
+private:
+  VISKORES_EXEC viskores::Float64 ComputeWeight(viskores::IdComponent distanceSquared) const
+  {
+    if (this->PatchRadius <= 0)
+    {
+      return 1.0;
+    }
+
+    const viskores::Float64 sigma = static_cast<viskores::Float64>(this->PatchRadius) / 3.0;
+    return viskores::Exp(-static_cast<viskores::Float64>(distanceSquared) / (2.0 * sigma * sigma));
+  }
+
+  viskores::IdComponent PatchRadius;
+};
+
+class SSIMStatisticsAdd : public viskores::worklet::WorkletMapField
+{
+public:
+  using ControlSignature = void(FieldInOut, FieldIn);
+  using ExecutionSignature = void(_1, _2);
+  using InputDomain = _1;
+
+  VISKORES_EXEC void operator()(SSIMStatistics& accumulation, const SSIMStatistics& newvalue) const
+  {
+    accumulation = accumulation + newvalue;
+  }
+};
+
+class SSIMFinalize : public viskores::worklet::WorkletMapField
+{
+public:
+  using ControlSignature = void(FieldIn, FieldOut);
+  using ExecutionSignature = void(_1, _2);
+  using InputDomain = _1;
+
+  VISKORES_CONT
+  SSIMFinalize(viskores::Float64 dynamicRange, viskores::Float64 k1, viskores::Float64 k2)
+    : DynamicRange(dynamicRange)
+    , K1(k1)
+    , K2(k2)
+  {
+  }
+
+  VISKORES_EXEC void operator()(const SSIMStatistics& statistics, viskores::Float64& ssim) const
+  {
+    if (statistics.Weight <= 0)
+    {
+      this->RaiseError("SSIM patch contains no samples.");
+      ssim = 0;
+      return;
+    }
+
+    const viskores::Float64 primaryMean = statistics.SumPrimary / statistics.Weight;
+    const viskores::Float64 secondaryMean = statistics.SumSecondary / statistics.Weight;
+    viskores::Float64 primaryVariance =
+      (statistics.SumPrimarySquared / statistics.Weight) - (primaryMean * primaryMean);
+    viskores::Float64 secondaryVariance =
+      (statistics.SumSecondarySquared / statistics.Weight) - (secondaryMean * secondaryMean);
+    const viskores::Float64 covariance =
+      (statistics.SumProduct / statistics.Weight) - (primaryMean * secondaryMean);
+
+    if (primaryVariance < 0)
+    {
+      primaryVariance = 0;
+    }
+    if (secondaryVariance < 0)
+    {
+      secondaryVariance = 0;
+    }
+
+    const viskores::Float64 c1 = (this->K1 * this->DynamicRange) * (this->K1 * this->DynamicRange);
+    const viskores::Float64 c2 = (this->K2 * this->DynamicRange) * (this->K2 * this->DynamicRange);
+
+    const viskores::Float64 luminance = (2 * primaryMean * secondaryMean) + c1;
+    const viskores::Float64 contrastStructure = (2 * covariance) + c2;
+    const viskores::Float64 denominator =
+      ((primaryMean * primaryMean) + (secondaryMean * secondaryMean) + c1) *
+      (primaryVariance + secondaryVariance + c2);
+
+    if (denominator == 0)
+    {
+      this->RaiseError("SSIM denominator is zero.");
+      ssim = 0;
+      return;
+    }
+
+    ssim = (luminance * contrastStructure) / denominator;
+  }
+
+  viskores::Float64 DynamicRange;
+  viskores::Float64 K1;
+  viskores::Float64 K2;
+};
+
+void IncludeFieldRange(const viskores::cont::Field& field, viskores::Range& fullRange)
+{
+  viskores::cont::ArrayHandle<viskores::Range> componentRanges = field.GetRange();
+  auto rangePortal = componentRanges.ReadPortal();
+  for (viskores::Id componentI = 0; componentI < rangePortal.GetNumberOfValues(); ++componentI)
+  {
+    fullRange.Include(rangePortal.Get(componentI));
+  }
+}
+
+viskores::Float64 ComputeFieldRange(const viskores::cont::Field& primaryField,
+                                    const viskores::cont::Field& secondaryField)
+{
+  viskores::Range fullRange;
+  IncludeFieldRange(primaryField, fullRange);
+  IncludeFieldRange(secondaryField, fullRange);
+  viskores::Float64 range = fullRange.Length();
+  return (range > 0) ? range : 1.0;
+}
+
+void ValidateParameters(viskores::IdComponent patchRadius,
+                        viskores::Float64 dynamicRange,
+                        viskores::Float64 k1,
+                        viskores::Float64 k2)
+{
+  if (patchRadius < 0)
+  {
+    throw viskores::cont::ErrorFilterExecution("SSIM patch radius must be nonnegative.");
+  }
+  if (dynamicRange <= 0)
+  {
+    throw viskores::cont::ErrorFilterExecution("SSIM dynamic range must be positive.");
+  }
+  if ((k1 < 0) || (k2 < 0))
+  {
+    throw viskores::cont::ErrorFilterExecution("SSIM constants K1 and K2 must be nonnegative.");
+  }
+}
+
+void ValidateFieldPair(const viskores::cont::DataSet& input,
+                       const viskores::cont::Field& primary,
+                       const viskores::cont::Field& secondary)
+{
+  if (!primary.IsPointField() || !secondary.IsPointField())
+  {
+    throw viskores::cont::ErrorFilterExecution(
+      "SSIM expects the primary and secondary fields to be point fields.");
+  }
+
+  if (primary.GetNumberOfValues() != secondary.GetNumberOfValues())
+  {
+    throw viskores::cont::ErrorFilterExecution(
+      "SSIM expects the primary and secondary fields to have the same number of values.");
+  }
+
+  if (primary.GetNumberOfValues() < 1)
+  {
+    throw viskores::cont::ErrorFilterExecution("SSIM expects at least one field value.");
+  }
+
+  if (primary.GetNumberOfValues() != input.GetNumberOfPoints())
+  {
+    throw viskores::cont::ErrorFilterExecution(
+      "SSIM expects point fields to have the same number of values as the input points.");
+  }
+
+  if (primary.GetData().GetNumberOfComponentsFlat() !=
+      secondary.GetData().GetNumberOfComponentsFlat())
+  {
+    throw viskores::cont::ErrorFilterExecution(
+      "SSIM expects the primary and secondary fields to have the same number of components.");
+  }
+
+  if (primary.GetData().GetNumberOfComponentsFlat() < 1)
+  {
+    throw viskores::cont::ErrorFilterExecution("SSIM expects at least one field component.");
+  }
+}
+
+template <typename PrimaryArrayType, typename SecondaryArrayType>
+void InvokeSSIMNeighborhood(const viskores::cont::UnknownCellSet& inputCellSet,
+                            const PrimaryArrayType& primary,
+                            const SecondaryArrayType& secondary,
+                            viskores::IdComponent patchRadius,
+                            viskores::Float64 dynamicRange,
+                            viskores::Float64 k1,
+                            viskores::Float64 k2,
+                            viskores::cont::ArrayHandle<viskores::Float64>& output)
+{
+  viskores::cont::Invoker invoke;
+
+  viskores::cont::ArrayHandle<SSIMStatistics> statistics;
+  const viskores::IdComponent numberOfComponents = primary.GetNumberOfComponents();
+  for (viskores::IdComponent component = 0; component < numberOfComponents; ++component)
+  {
+    viskores::cont::ArrayHandle<SSIMStatistics> componentStatistics;
+    invoke(SSIMComponentNeighborhood(patchRadius),
+           inputCellSet,
+           primary.GetComponentArray(component),
+           secondary.GetComponentArray(component),
+           componentStatistics);
+
+    if (component == 0)
+    {
+      statistics = componentStatistics;
+    }
+    else
+    {
+      invoke(SSIMStatisticsAdd{}, statistics, componentStatistics);
+    }
+  }
+
+  invoke(SSIMFinalize(dynamicRange, k1, k2), statistics, output);
+}
+
+viskores::cont::ArrayHandle<viskores::Float64> ComputeSSIMArray(
+  const viskores::cont::DataSet& input,
+  const viskores::cont::Field& primaryField,
+  const viskores::cont::Field& secondaryField,
+  viskores::IdComponent patchRadius,
+  viskores::Float64 dynamicRange,
+  viskores::Float64 k1,
+  viskores::Float64 k2)
+{
+  ValidateFieldPair(input, primaryField, secondaryField);
+  ValidateParameters(patchRadius, dynamicRange, k1, k2);
+
+  auto inputCellSet =
+    input.GetCellSet().ResetCellSetList<VISKORES_DEFAULT_CELL_SET_LIST_STRUCTURED>();
+
+  viskores::cont::ArrayHandle<viskores::Float64> output;
+  const auto& primaryData = primaryField.GetData();
+  const auto& secondaryData = secondaryField.GetData();
+  auto resolvePrimary = [&](const auto& primaryArray)
+  {
+    using PrimaryArrayType = std::decay_t<decltype(primaryArray)>;
+    using ComponentType = typename PrimaryArrayType::ValueType::ComponentType;
+
+    if (secondaryData.IsBaseComponentType<ComponentType>())
+    {
+      InvokeSSIMNeighborhood(inputCellSet,
+                             primaryArray,
+                             secondaryData.ExtractArrayFromComponents<ComponentType>(),
+                             patchRadius,
+                             dynamicRange,
+                             k1,
+                             k2,
+                             output);
+    }
+    else
+    {
+      const viskores::cont::UnknownArrayHandle primaryFloat = primaryField.GetDataAsDefaultFloat();
+      const viskores::cont::UnknownArrayHandle secondaryFloat =
+        secondaryField.GetDataAsDefaultFloat();
+      InvokeSSIMNeighborhood(inputCellSet,
+                             primaryFloat.ExtractArrayFromComponents<viskores::FloatDefault>(),
+                             secondaryFloat.ExtractArrayFromComponents<viskores::FloatDefault>(),
+                             patchRadius,
+                             dynamicRange,
+                             k1,
+                             k2,
+                             output);
+    }
+  };
+  primaryData.CastAndCallWithExtractedArray(resolvePrimary);
+
+  return output;
+}
+
+} // anonymous namespace
+
+namespace viskores
+{
+namespace filter
+{
+namespace image_processing
+{
+
+//-----------------------------------------------------------------------------
+SSIM::SSIM()
+{
+  this->SetPrimaryField("image-1");
+  this->SetSecondaryField("image-2");
+  this->SetOutputFieldName("ssim");
+}
+
+//-----------------------------------------------------------------------------
+viskores::Float64 SSIM::GetRealDynamicRange(const viskores::cont::Field& primaryField,
+                                            const viskores::cont::Field& secondaryField) const
+{
+  viskores::Float64 dynamicRange;
+  switch (this->RangeType)
+  {
+    case DynamicRangeType::Default:
+    {
+      dynamicRange = 0;
+      bool foundIntegralRange = false;
+      auto checkFieldType = [&](auto type)
+      {
+        using Type = decltype(type);
+        if (!foundIntegralRange && primaryField.GetData().IsBaseComponentType<Type>())
+        {
+          if constexpr (std::is_integral_v<Type>)
+          {
+            dynamicRange = static_cast<viskores::Float64>(std::numeric_limits<Type>::max()) -
+              static_cast<viskores::Float64>(std::numeric_limits<Type>::min());
+            foundIntegralRange = true;
+          }
+        }
+      };
+      viskores::ListForEach(checkFieldType, viskores::TypeListBaseC{});
+
+      if (!foundIntegralRange)
+      {
+        dynamicRange = ComputeFieldRange(primaryField, secondaryField);
+      }
+    }
+    break;
+    case DynamicRangeType::Explicit:
+      dynamicRange = this->DynamicRange;
+      break;
+    case DynamicRangeType::Range:
+      dynamicRange = ComputeFieldRange(primaryField, secondaryField);
+      break;
+  }
+
+  return dynamicRange;
+}
+
+//-----------------------------------------------------------------------------
+viskores::Float64 SSIM::ComputeMetric(const viskores::cont::DataSet& input) const
+{
+  const auto& primaryField = this->GetFieldFromDataSet(input);
+  const auto& secondaryField = this->GetFieldFromDataSet(1, input);
+
+  const auto ssimArray = ComputeSSIMArray(input,
+                                          primaryField,
+                                          secondaryField,
+                                          this->PatchRadius,
+                                          this->GetRealDynamicRange(primaryField, secondaryField),
+                                          this->K1,
+                                          this->K2);
+  return viskores::cont::Algorithm::Reduce(ssimArray, 0.0) /
+    static_cast<viskores::Float64>(ssimArray.GetNumberOfValues());
+}
+
+//-----------------------------------------------------------------------------
+viskores::cont::DataSet SSIM::DoExecute(const viskores::cont::DataSet& input)
+{
+  const auto& primaryField = this->GetFieldFromDataSet(input);
+  const auto& secondaryField = this->GetFieldFromDataSet(1, input);
+
+  auto ssimArray = ComputeSSIMArray(input,
+                                    primaryField,
+                                    secondaryField,
+                                    this->PatchRadius,
+                                    this->GetRealDynamicRange(primaryField, secondaryField),
+                                    this->K1,
+                                    this->K2);
+
+  std::string outputFieldName = this->GetOutputFieldName();
+  if (outputFieldName.empty())
+  {
+    outputFieldName = "ssim";
+  }
+  return this->CreateResultFieldPoint(input, outputFieldName, ssimArray);
+}
+
+} // namespace image_processing
+} // namespace filter
+} // namespace viskores
---
+++ viskores/filter/image_processing/SSIM.h
@@ -0,0 +1,231 @@
+//============================================================================
+//  The contents of this file are covered by the Viskores license. See
+//  LICENSE.txt for details.
+//
+//  By contributing to this file, all contributors agree to the Developer
+//  Certificate of Origin Version 1.1 (DCO 1.1) as stated in DCO.txt.
+//============================================================================
+
+#ifndef viskores_filter_image_processing_SSIM_h
+#define viskores_filter_image_processing_SSIM_h
+
+#include <viskores/filter/Filter.h>
+#include <viskores/filter/image_processing/viskores_filter_image_processing_export.h>
+
+namespace viskores
+{
+namespace filter
+{
+namespace image_processing
+{
+
+/// \brief Computes the Structural Similarity Index Measure (SSIM) between two image fields.
+///
+/// This filter compares the active primary and secondary fields with the
+/// standard SSIM formula over a local patch around each point:
+/// `((2*mean_x*mean_y + c1) * (2*cov_xy + c2)) /
+///  ((mean_x^2 + mean_y^2 + c1) * (var_x + var_y + c2))`.
+///
+/// The primary and secondary fields must be point fields on a structured data
+/// set. `PatchRadius()` controls the radius of the local patch used for each
+/// output point. Scalar fields are compared directly. Color or vector fields are
+/// compared by flattening their components within each local patch. Neighboring
+/// samples within the patch are weighted by a Gaussian based on index distance.
+///
+/// The result is a `viskores::cont::DataSet` containing a point field named
+/// "ssim" by default. Use `ComputeMetric()` to get the average of the pointwise
+/// SSIM values.
+class VISKORES_FILTER_IMAGE_PROCESSING_EXPORT SSIM : public viskores::filter::Filter
+{
+public:
+  /// @brief Construct an SSIM filter.
+  ///
+  /// The default primary field name is "image-1", the default secondary field
+  /// name is "image-2", the default output field name is "ssim", and the
+  /// default patch radius is 3.
+  VISKORES_CONT SSIM();
+
+  /// @brief Choose the primary field to compare.
+  ///
+  /// The primary field must have the same association, number of values, and
+  /// number of components as the secondary field.
+  VISKORES_CONT
+  void SetPrimaryField(
+    const std::string& name,
+    viskores::cont::Field::Association association = viskores::cont::Field::Association::Any)
+  {
+    this->SetActiveField(name, association);
+  }
+
+  /// @brief Get the name of the primary field.
+  VISKORES_CONT const std::string& GetPrimaryFieldName() const
+  {
+    return this->GetActiveFieldName();
+  }
+
+  /// @brief Get the association used to select the primary field.
+  VISKORES_CONT viskores::cont::Field::Association GetPrimaryFieldAssociation() const
+  {
+    return this->GetActiveFieldAssociation();
+  }
+
+  /// @brief Choose the secondary field to compare.
+  ///
+  /// The secondary field must have the same association, number of values, and
+  /// number of components as the primary field.
+  VISKORES_CONT
+  void SetSecondaryField(
+    const std::string& name,
+    viskores::cont::Field::Association association = viskores::cont::Field::Association::Any)
+  {
+    this->SetActiveField(1, name, association);
+  }
+
+  /// @brief Get the name of the secondary field.
+  VISKORES_CONT const std::string& GetSecondaryFieldName() const
+  {
+    return this->GetActiveFieldName(1);
+  }
+
+  /// @brief Get the association used to select the secondary field.
+  VISKORES_CONT viskores::cont::Field::Association GetSecondaryFieldAssociation() const
+  {
+    return this->GetActiveFieldAssociation(1);
+  }
+
+  /// @brief Enumeration of modes to select the dynamic range.
+  ///
+  /// The SSIM algorithm operates based on the range of _possible_ values that
+  /// can be held by either field. This range can either be guessed or set
+  /// explicitly. This enumerator is used to select how the dynamic range is
+  /// specified.
+  enum struct DynamicRangeType
+  {
+    /// @copydoc SetDynamicRangeToDefault
+    Default,
+    /// @copydoc SetDynamicRangeToExplicit
+    Explicit,
+    /// @copydoc SetDynamicRangeToRange
+    Range
+  };
+
+  /// @brief Use a default value for the dynamic range based on the field type.
+  ///
+  /// If the field type is an integer, the dynamic range will be set to the range
+  /// of possible values for that integer (e.g., 255 for 8-bit integers or 65535
+  /// for 16-bit integers). If the field type is a float, the dynamic range will
+  /// be set to the computed range of the input fields. If that computed range is
+  /// zero, the dynamic range falls back to 1.0. This is the default behavior of
+  /// the SSIM filter.
+  VISKORES_CONT void SetDynamicRangeToDefault()
+  {
+    this->SetDynamicRangeType(DynamicRangeType::Default);
+  }
+
+  /// @brief Use an explicit value for the dynamic range.
+  ///
+  /// This behavior for the dynamic range requires the user to set an explicit
+  /// value for the range. This value is set with the
+  /// `viskores::filter::image_processing::SetDynamicRange()` method.
+  VISKORES_CONT void SetDynamicRangeToExplicit()
+  {
+    this->SetDynamicRangeType(DynamicRangeType::Explicit);
+  }
+
+  /// @brief Use the range of the input fields for the dynamic range.
+  ///
+  /// This behavior for the dynamic range queries the input fields for the range
+  /// they contain. This range is used as the dynamic range. Note that this
+  /// assumes that the fields contain the minimum and maximum possible values,
+  /// which is not always the case.
+  VISKORES_CONT void SetDynamicRangeToRange()
+  {
+    this->SetDynamicRangeType(DynamicRangeType::Range);
+  }
+
+  /// @brief The method for specifying the dynamic range of the field values.
+  ///
+  /// The SSIM algorithm operates based on the range of _possible_ values that
+  /// can be held by either field. This range can either be guessed or set
+  /// explicitly and the type specifies how this is determined.
+  VISKORES_CONT void SetDynamicRangeType(DynamicRangeType rangeType)
+  {
+    this->RangeType = rangeType;
+  }
+  /// @copydoc SetDynamicRangeType
+  VISKORES_CONT DynamicRangeType GetDynamicRangeType() const { return this->RangeType; }
+
+  /// @brief Set the dynamic range used to compute SSIM constants.
+  ///
+  /// The SSIM algorithm operates based on the range of _possible_ values that
+  /// can be held by either field. Calling this method sets what this range is.
+  /// Calling this method changes the dynamic range type to `Explicit`.
+  VISKORES_CONT void SetDynamicRange(viskores::Float64 dynamicRange)
+  {
+    this->DynamicRange = dynamicRange;
+    this->SetDynamicRangeToExplicit();
+  }
+
+  /// @brief Get the dynamic range used to compute SSIM constants.
+  VISKORES_CONT viskores::Float64 GetDynamicRange() const { return this->DynamicRange; }
+
+  /// @brief The K1 SSIM stabilization constant.
+  ///
+  /// The `K1` constant is a small number used by SSIM to stabilize the metric
+  /// for dark, flat, or low-contrast areas of the image and to prevent division
+  /// by zero. The default value for `K1` is 0.01, which is customary for SSIM
+  /// calculations.
+  VISKORES_CONT void SetK1(viskores::Float64 k1) { this->K1 = k1; }
+
+  /// @copydoc SetK1
+  VISKORES_CONT viskores::Float64 GetK1() const { return this->K1; }
+
+  /// @brief Set the K2 SSIM stabilization constant.
+  ///
+  /// The `K2` constant is a small number used by SSIM to stabilize the metric
+  /// for low-contrast or low-variance areas of the image and to prevent division
+  /// by zero. The default value for `K2` is 0.03, which is customary for SSIM
+  /// calculations.
+  VISKORES_CONT void SetK2(viskores::Float64 k2) { this->K2 = k2; }
+
+  /// @copydoc SetK2
+  VISKORES_CONT viskores::Float64 GetK2() const { return this->K2; }
+
+  /// @brief Set the radius of the local SSIM patch.
+  ///
+  /// A radius of 0 uses only the point itself. A positive radius includes
+  /// neighboring points within a circular or spherical patch in structured
+  /// image-index space. The default radius is 3.
+  VISKORES_CONT void SetPatchRadius(viskores::IdComponent patchRadius)
+  {
+    this->PatchRadius = patchRadius;
+  }
+
+  /// @brief Get the radius of the local SSIM patch.
+  VISKORES_CONT viskores::IdComponent GetPatchRadius() const { return this->PatchRadius; }
+
+  /// @brief Compute and return the SSIM value for the configured fields.
+  ///
+  /// This method computes the local SSIM point field and returns the average of
+  /// those values without creating an output `viskores::cont::DataSet`.
+  VISKORES_CONT viskores::Float64 ComputeMetric(const viskores::cont::DataSet& input) const;
+
+private:
+  VISKORES_CONT viskores::cont::DataSet DoExecute(const viskores::cont::DataSet& input) override;
+
+  VISKORES_CONT viskores::Float64 GetRealDynamicRange(
+    const viskores::cont::Field& primaryField,
+    const viskores::cont::Field& secondaryField) const;
+
+  DynamicRangeType RangeType = DynamicRangeType::Default;
+  viskores::Float64 DynamicRange = 1.0;
+  viskores::Float64 K1 = 0.01;
+  viskores::Float64 K2 = 0.03;
+  viskores::IdComponent PatchRadius = 3;
+};
+
+} // namespace image_processing
+} // namespace filter
+} // namespace viskores
+
+#endif // viskores_filter_image_processing_SSIM_h
